083b1d090ccac8984ca4a599479581343c36871e
98teg <teg98tom@gmail.com>
2019-05-15 22:53:12 +0200

    Format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User
   field :name, type: String
   field :email, type: String
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 },
+					format: { with: VALID_EMAIL_REGEX }
 end
