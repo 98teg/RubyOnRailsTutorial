e8dbb5ccd3d0ee1d5e83682d780fdecd575cba54
98teg <teg98tom@gmail.com>
2019-05-17 12:57:13 +0200

    Finsih user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-
+			flash[:success] = "Bienvenido a la : App de Ejemplo"
+			redirect_to @user
 		else
 			render 'new'
 		end
