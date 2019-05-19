7eb934e071431725ae9781b1afbc49ea0c5d0923
98teg <teg98tom@gmail.com>
2019-05-19 22:16:51 +0200

    Delete users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-	  @base_title = "Sample Application"
+    @base_title = "Sample Application"
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    @user.save
+
+    @admin = User.new(name: "Admin User", email: "admin@example.com",
+                     password: "foobar", password_confirmation: "foobar", admin: "true")
+    @admin.save
   end
 
   test "should get new" do
@@ -16,4 +23,35 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@user, password: "foobar")
+    assert_not @user.admin
+    patch user_path(@user), params: {
+                              user: { password: "foobar",
+                                      password_confirmation: "foobar",
+                                      admin: true } }
+    assert_not @user.reload.admin
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user, password: "foobar")
+    end
+
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@user, password: "foobar")
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+
+    assert_redirected_to root_url
+  end
+
+  def teardown
+    @user.destroy
+    @admin.destroy
+  end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,9 +3,17 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-		30.times do |n|
-			User.create(name: "User #{n}", email: "user-#{n}@example.com",
-									password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    @user.save
+
+    @admin = User.new(name: "Admin User", email: "admin@example.com",
+                     password: "foobar", password_confirmation: "foobar", admin: "true")
+    @admin.save
+
+    30.times do |n|
+      User.create(name: "User #{n}", email: "user-#{n}@example.com",
+                  password: "foobar", password_confirmation: "foobar")
     end
   end
 
@@ -17,6 +25,30 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.page(1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin, password: "foobar")
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.page(1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@user)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@user, password: "foobar")
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 
 	def teardown
