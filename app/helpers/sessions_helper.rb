0abf16db033988bff2aa7be8497815a7350d0468
98teg <teg98tom@gmail.com>
2019-05-19 17:50:08 +0200

    Users can edit their profile info

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,6 +28,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -35,8 +39,17 @@ module SessionsHelper
   end
 
   def log_out
-	forget(current_user)
-	session.delete(:user_id)
-	@current_user = nil
+	  forget(current_user)
+	  session.delete(:user_id)
+	  @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwading_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
