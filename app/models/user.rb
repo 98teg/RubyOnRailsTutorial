d62b6a4cbc789b683d990a158acd4c554771f647
98teg <teg98tom@gmail.com>
2019-05-21 19:46:48 +0200

    Creating password reseting forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,10 @@ class User
   field :activated, type: Boolean, default: false
   field :activation_digest, type: String
   field :activated_at, type: Time
+  field :reset_digest, type: String
+  field :reset_sent_at, type: Time
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -47,6 +49,16 @@ class User
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   def remember
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,4 +15,18 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email),  mail.body.encoded
     user.destroy
   end
+
+  test "password_reset" do
+    user = User.create(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+    user.save
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+    user.destroy
+  end
 end
