e1ccb9c42a2fd7600ffdd8465744e584e469eb0a
98teg <teg98tom@gmail.com>
2019-05-21 18:11:45 +0200

    Email implementation added

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	test "valid signup information" do
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
+	test "valid signup information with account activation" do
 		get signup_path
 		assert_difference 'User.count', 1 do
 			post signup_path, params: { user: { name:  "Example User",
@@ -9,11 +14,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 				                                password:              "password",
 				                                password_confirmation: "password" } }
 		end
-
-		follow_redirect!
-		#assert_template 'users/show'
-	    #assert_not flash.empty?
-		#assert is_logged_in?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    #Try to log in without activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    #Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    #Valid token, wrong mail
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    #Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
 	end
 
 
@@ -26,11 +44,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 											   password_confirmation: "bar" } }
 		end
 		assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
 	end
 
-  	def teardown
-    	if !User.where(email: "user@example.com").empty?
-			User.find_by(email: "user@example.com").destroy
-		end
-  	end
+  def teardown
+   	if !User.where(email: "user@example.com").empty?
+		  User.find_by(email: "user@example.com").destroy
+	  end
+  end
 end	
