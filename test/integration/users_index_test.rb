f55cc5a812e92e635b34d753e53cc5ce2f650717
98teg <teg98tom@gmail.com>
2019-05-27 00:04:51 +0200

    Fixing a test error

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -39,7 +39,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+        assert_select 'a[href=?]', user_path(user), text: 'eliminar'
       end
     end
 
